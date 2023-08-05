c8ccdc0a47a8b27acb9bfc64d07e6bd4909f7c97
SnubStumpy2007 <joshua192007@gmail.com>
2023-08-05 17:07:12 -0400

    update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,106 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product } = require('../../models'); // Import Category and Product models
 
-// The `/api/categories` endpoint
-
+// GET all categories and their associated products
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // Find all categories along with their associated products using Sequelize's findAll method
+    Category.findAll({  // This reads the whole Category table from the database https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#simple-select-queries
+        include: {
+            model: Product, // Include the Product model
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id'] // Specify the attributes to include
+        }
+    })
+    .then(dbCatData => {
+        if (!dbCatData) {
+            res.status(404).json({ message: 'No categories found' }); // Handle case when no categories are found
+            return;
+        }
+        res.json(dbCatData); // Send the retrieved data as JSON response
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err); // Handle errors with a 500 response
+    });
 });
 
+// GET a specific category by ID along with its associated products
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    // Find a specific category by its ID along with associated products using Sequelize's findOne method
+    Category.findOne({  // Same thing as findAll, but looking for a specific category
+        where: {
+            id: req.params.id
+        },
+        include: {
+            model: Product, // Include the Product model
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id'] // Specify the attributes to include
+        }
+    })
+    .then(dbCatData => {
+        if (!dbCatData) {
+            res.status(404).json({ message: 'No categories found' }); // Handle case when no categories are found
+            return;
+        }
+        res.json(dbCatData); // Send the retrieved data as JSON response
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err); // Handle errors with a 500 response
+    });
 });
 
+// Create a new category
 router.post('/', (req, res) => {
-  // create a new category
+    // Create a new category using Sequelize's create method
+    Category.create({
+        category_name: req.body.category_name // Get category_name from the request body
+    })
+    .then(dbCatData => res.json(dbCatData)) // Send the created category data as JSON response.  dbCatData stands for Database Category Data
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err); // Handle errors with a 500 response
+    });
 });
 
+// Update a category by ID
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // Update a category by its ID using Sequelize's update method
+    Category.update(req.body, {  // update querys.
+        where: {
+            id: req.params.id // Use the provided ID parameter to identify the category to update
+        }
+    })
+    .then(dbCatData => {
+        if (!dbCatData) {
+            res.status(404).json({ message: 'No category found with this id' }); // Handle case when category is not found
+            return;
+        }
+        res.json(dbCatData); // Send the updated category data as JSON response
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err); // Handle errors with a 500 response
+    });
 });
 
+// Delete a category by ID
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // Delete a category by its ID using Sequelize's destroy method
+    Category.destroy({  // Delete queries where etc...
+        where: {
+            id: req.params.id // Use the provided ID parameter to identify the category to delete
+        }
+    })
+    .then(dbCatData => {
+        if (!dbCatData) {
+            res.status(404).json({ message: 'No category found with that id.' }); // Handle case when category is not found
+            return;
+        }
+        res.json(dbCatData); // Send the deleted category data as JSON response
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err); // Handle errors with a 500 response
+    });
 });
 
 module.exports = router;
